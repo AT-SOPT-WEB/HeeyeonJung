832fa7ce796a38555bc841580816dd4c7b8766e9
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-01 15:34:45 +0900

    ✨ feat: 깃허브 검색 userInfo status에 따른 분기처리 구현

--- week3-assignment/src/components/GithubUser/GithubSearch.jsx
+++ week3-assignment/src/components/GithubUser/GithubSearch.jsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import { useGithubUserInfo } from '../../hooks/useGithubUserInfo';
 import StyledInput from '../common/StyledInput';
 import GithubUserCard from './GithubUserCard';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function GithubSearch() {
   const [input, setInput] = useState('');
@@ -24,8 +25,13 @@ function GithubSearch() {
         placeholder="깃허브 아이디를 입력하세요"
       />
 
-      {userInfo.status === 'pending' && <p>검색 중...</p>}
-      {userInfo.status === 'rejected' && <ErrorText>사용자를 찾을 수 없습니다.</ErrorText>}
+      {userInfo.status === 'pending' && (
+        <SpinnerWrapper>
+          <ClipLoader color="#a3b18a" size={200} />
+        </SpinnerWrapper>
+      )}
+
+      {userInfo.status === 'rejected' && <ErrorText>사용자를 찾을 수 없습니다. 다시 시도해주세요.</ErrorText>}
 
       {userInfo.status === 'resolved' && userInfo.data && (
         <GithubUserCard user={userInfo.data} onClose={() => getUserInfo('')} />
@@ -44,4 +50,12 @@ const Container = styled.div`
 
 const ErrorText = styled.p`
   color: red;
+  margin-top: 2rem;
+`;
+
+const SpinnerWrapper = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
