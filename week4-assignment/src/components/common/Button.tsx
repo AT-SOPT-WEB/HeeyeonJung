c5b66107ba9d6fb57c3bb2ea4ae689811a90990c
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-12 20:59:02 +0900

    ✨ feat: 버튼 컴포넌트 활성화 기능 추가

--- week4-assignment/src/components/common/Button.tsx
+++ week4-assignment/src/components/common/Button.tsx
@@ -5,21 +5,31 @@ interface ButtonProps {
   textColorClass?: string;
   className?: string;
   type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 }
 
 const Button = ({
   text,
   onClick,
-  colorClass = 'bg-buttonDefault',
+  colorClass = 'bg-buttonActive',
   textColorClass = 'text-white',
   className = '',
   type = 'button',
+  disabled = false,
 }: ButtonProps) => {
+  const appliedColorClass = disabled ? 'bg-buttonDefault' : colorClass;
+
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`w-full max-w-lg py-2 mb-2 rounded-lg hover:opacity-90 ${colorClass} ${textColorClass} ${className}`}
+      disabled={disabled}
+      className={`
+        w-full max-w-lg py-2 mb-2 rounded-lg
+        ${appliedColorClass} ${textColorClass}
+        ${disabled ? 'cursor-not-allowed' : 'hover:opacity-90'}
+        ${className}
+      `}
     >
       {text}
     </button>
