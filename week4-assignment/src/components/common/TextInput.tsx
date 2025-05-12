3d141e85ff4f935a3db6e46343743960de2d1fc4
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-12 21:01:24 +0900

    ✨ feat: TextInput 비밀번호 보기 토글 기능 추가

--- week4-assignment/src/components/common/TextInput.tsx
+++ week4-assignment/src/components/common/TextInput.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import { useState } from 'react';
 
 interface TextInputProps {
   type?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
+  showToggleIcon?: boolean;
 }
 
-const TextInput = ({ type = 'text', value, onChange, placeholder }: TextInputProps) => {
+const TextInput = ({
+  type = 'text',
+  value,
+  onChange,
+  placeholder,
+  showToggleIcon = false, 
+}: TextInputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === 'password';
+
+  const inputType = isPassword
+    ? showToggleIcon && showPassword
+      ? 'text'
+      : 'password'
+    : type;
+
   return (
-    <input
-      type={type}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      className="w-full max-w-lg px-4 py-3 mb-4 border rounded-lg text-sm"
-    />
+    <div className="relative w-full max-w-lg">
+      <input
+        type={inputType}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full px-4 py-3 mb-4 border rounded-lg text-sm"
+      />
+      {isPassword && showToggleIcon && (
+        <div
+          className="absolute right-4 top-3.5  cursor-pointer text-gray-400"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+        </div>
+      )}
+    </div>
   );
 };
 
