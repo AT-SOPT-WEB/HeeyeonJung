4f5c58121fe48b2942ae246e5963a2f01ed51c66
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-13 16:56:44 +0900

    ✨ feat: api 응답 확인용 콘솔 추가

--- week4-assignment/src/apis/authApi.ts
+++ week4-assignment/src/apis/authApi.ts
@@ -8,8 +8,8 @@ export const signup = async (loginId: string, password: string, nickname: string
       password,
       nickname,
     });
-
-    return response.data;
+     console.log('✅ 회원가입 응답:', response);
+     return response.data.data;
   } catch (error) {
     const err = error as AxiosError<{ message: string }>;
     throw new Error(err.response?.data?.message || '회원가입 중 오류가 발생했습니다.');
@@ -22,8 +22,8 @@ export const login = async (loginId: string, password: string) => {
       loginId,
       password,
     });
-
-    return response.data; 
+     console.log('✅ 로그인 응답:', response);
+     return response.data.data;
   } catch (error) {
     const err = error as AxiosError<{ message: string }>;
     throw new Error(err.response?.data?.message || '로그인 중 오류가 발생했습니다.');
